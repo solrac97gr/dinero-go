df960fc8d2cfc37123eb093542e85b32357f8c16
carlos97gr <carlosgperu97@gmail.com>
2022-03-03 20:24:07 +0300

    Test and method implement add validation for Currency codes

--- dinero.go
+++ dinero.go
@@ -1,13 +1,31 @@
 package dinerogo
 
+import "errors"
+
 // Dinero: Monetary object representation
 type Dinero struct {
-	Amount float64 //Amount: The minium unity of the currency (USD=cents)
+	Amount   float64 //Amount: The minium unity of the currency (1USD=100cents)
+	Currency string  // Currency : Use the ISO4217 format (USD)
 }
 
 // NewDinero : Function for create a new Dinero
-func NewDinero(amount float64) (*Dinero, error) {
+func NewDinero(amount float64, currency string) (*Dinero, error) {
+	if !IsValidCurrency(currency) {
+		return nil, errors.New("the currency is not valid in ISO4217")
+	}
 	return &Dinero{
-		Amount: amount,
+		Amount:   amount,
+		Currency: currency,
 	}, nil
+}
+
+// IsValidCurrency : Check if the currency is valid
+func IsValidCurrency(alphabeticCode string) bool {
+	for _, currency := range ValidCurrency {
+		code := currency.GetAlphabeticCode()
+		if code == alphabeticCode {
+			return true
+		}
+	}
+	return false
 }
--- methods.go
+++ methods.go
@@ -3,3 +3,7 @@ package dinerogo
 func (d *Dinero) GetAmount() float64 {
 	return d.Amount
 }
+
+func (d *Dinero) GetCurrency() string {
+	return d.Currency
+}
