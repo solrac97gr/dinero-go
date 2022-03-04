d4d1faefc6fc871c0c6e2b90a1dbbfb1122c6719
carlos97gr <carlosgperu97@gmail.com>
2022-03-04 16:22:51 +0300

    add dinero with test for ideal case implemented

--- methods.go
+++ methods.go
@@ -20,15 +20,6 @@ func (d *Dinero) GetLocale() string {
 	return d.Locale
 }
 
-// IsZero : Valid if the amount inside of Dinero obj it's 0
-func (d *Dinero) IsZero() bool {
-	if d.Amount == 0 {
-		return true
-	} else {
-		return false
-	}
-}
-
 // SetLocale : Change the locale language of the Dinero obj using valid language tag (Spanish=es)
 func (d *Dinero) SetLocale(locale string) error {
 	if IsValidLocale(locale) {
@@ -43,7 +34,7 @@ func (d *Dinero) GetPrecision() uint8 {
 	return d.Precision
 }
 
-//ConvertPrecision :
+//ConvertPrecision : Convert the decimal precision and the amount with a new precision
 func (d *Dinero) ConvertPrecision(newPrecision uint8) *Dinero {
 	if d.Precision != 0 {
 		return &Dinero{
@@ -58,5 +49,48 @@ func (d *Dinero) ConvertPrecision(newPrecision uint8) *Dinero {
 		Currency:  d.Currency,
 		Locale:    d.Locale,
 		Precision: newPrecision,
+	}
+}
+
+// Add : Add a Dinero object to another Dinero object
+func (d *Dinero) Add(dinero *Dinero) (*Dinero, error) {
+	if d.Currency != dinero.Currency {
+		return &Dinero{}, errors.New("dinero obj must be same currency")
+	}
+	if d.Precision == dinero.Precision {
+		return &Dinero{
+			Amount:    d.GetAmount() + dinero.GetAmount(),
+			Currency:  d.Currency,
+			Locale:    d.Locale,
+			Precision: d.Precision,
+		}, nil
+	}
+
+	if dinero.Precision > d.Precision {
+		newDinero := d.ConvertPrecision(dinero.Precision)
+		return &Dinero{
+			Amount:    dinero.GetAmount() + newDinero.GetAmount(),
+			Currency:  d.Currency,
+			Locale:    d.Locale,
+			Precision: dinero.Precision,
+		}, nil
+	}
+
+	newDinero := dinero.ConvertPrecision(d.Precision)
+	return &Dinero{
+		Amount:    d.GetAmount() + newDinero.GetAmount(),
+		Currency:  d.Currency,
+		Locale:    d.Locale,
+		Precision: d.Precision,
+	}, nil
+
+}
+
+// IsZero : Valid if the amount inside of Dinero obj it's 0
+func (d *Dinero) IsZero() bool {
+	if d.Amount == 0 {
+		return true
+	} else {
+		return false
 	}
 }
